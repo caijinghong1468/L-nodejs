6f041f8f1600aced99c7f58c37e69875777d0a81
Caijinghong <caijinghong@CaijinghongdeMacBook-Air.local>
2024-06-30 19:41:33 +0800

    start to learn Express , use template engines  to implement the View of MVC, write some README.md

---
+++ nodejs/my.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 5001;
+
+// 設定 view engine
+app.set('view engine', 'ejs')
+
+// 建立 todos data
+const todos = [
+  'first todo', 'second todo', 'third todo'
+]
+
+app.get('/todos', (req, res) => {
+  // 第二個參數可傳入資料
+  res.render('todos', {
+    todos     // todos: todos 一樣的話可省略寫法
+  })
+})
+
+app.get('/todos/:id',(req,res )=>{
+     // params: 可拿到網址列上指定的參數
+     const id = req.params.id
+     const todo = todos[id]
+    res.render('todo', {
+        todo
+    })
+})
+app.get('/hello', (req, res) => {
+  res.render('hello')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
