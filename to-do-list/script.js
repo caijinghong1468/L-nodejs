7512bef9aff35ffd9c80e633406d3a448e13ab4f
caijinghong1468 <andycow1468@gmail.com>
2024-08-17 11:35:06 +0800

    not finished script

---
+++ to-do-list/script.js
@@ -0,0 +1,11 @@
+function addItem(){
+    const ul = document.getElementById("list");
+    const input = document.getElementById("");
+    
+}
+
+const addBotton = document.getElementById("add-button");
+addBotton.addEventListener("click",addItem);
+
+const form = document.getElementById("input-wrapper");
+form.addEventListener("submit",(e)=>{e.preventDefault();});
